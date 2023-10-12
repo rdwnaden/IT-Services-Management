c5355c8aaa474b6e95694739adce0f71d89d444f
rdwnaden <ridwanaden1@gmail.com>
2023-10-12 16:40:47 +0700

    asset repair - add sequence

--- models/services.py
+++ models/services.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 
 
 class Services(models.Model):
@@ -8,7 +8,7 @@ class Services(models.Model):
     _inherit = ["mail.thread","mail.activity.mixin"]
     _description = 'Services'
 
-    name = fields.Char('Part Name', tracking=True)
+    name = fields.Char('Form Number', required=True, copy=False, readonly=True, index=True, default=lambda self: _('New'),)
     services_date = fields.Date('Services Date', tracking=True)
     description = fields.Text('Description', tracking=True)
     asset_id = fields.Many2one('itsm.asset', string='Asset Number', tracking=True)
@@ -19,6 +19,14 @@ class Services(models.Model):
         ('approved', 'Approved'),
         ('rejected', 'Rejected')
     ], string='Status', default = 'draft', tracking=True)
+
+    @api.model
+    def create(self, vals):
+        seq_date = None
+        if vals.get('name', _('New')) == _('New'):
+            vals['name'] = self.env['ir.sequence'].next_by_code('itsm.services') or _('New')
+        result = super(Services, self).create(vals)
+        return result
 
     def action_confirm(self):
         self.state = 'waiting_approval'
